bddd2852657d4ca2699d47e5d556527587452ebb
Kiran Gangadharan <me@kirang.in>
2026-04-17 11:39:07 +0530

    chore: remove vim and elixir packages

--- elixir/.iex.exs
+++
@@ -1,9 +0,0 @@
-Application.put_env(:elixir, :ansi_enabled, true)
-
-IEx.configure(
-  colors: [
-    eval_result: [:green, :bright] ,
-    eval_error: [[:red, :bright]],
-    eval_info: [:yellow, :bright ],
-  ]
-)
